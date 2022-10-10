22bf3d6c8d19604f6d4cbab93758161655ec8248
김형겸 <97586683+Brokyeom@users.noreply.github.com>
2022-10-10 18:20:55 +0900

    feat : nav 리스트 표시 기능 구현

--- week2/todo-list/TodoList.js
+++ week2/todo-list/TodoList.js
@@ -1,6 +1,10 @@
 const todayButton = document.querySelector(".today-button");
 const tomorrowButton = document.querySelector(".tomorrow-button");
 const allButton = document.querySelector(".all-button");
+const rightSection = document.querySelector(".right-section");
+const leftSection = document.querySelector(".left-section");
+
+const HIDDEN_CLASSNAME = "hidden";
 
 todayButton.addEventListener("click", clickToday);
 tomorrowButton.addEventListener("click", clickTomorrow);
@@ -8,12 +12,18 @@ allButton.addEventListener("click", clickAll);
 
 function clickToday() {
   console.log("오늘!");
+  rightSection.classList.add(HIDDEN_CLASSNAME);
+  leftSection.classList.remove(HIDDEN_CLASSNAME);
 }
 
 function clickTomorrow() {
   console.log("내일!");
+  leftSection.classList.add(HIDDEN_CLASSNAME);
+  rightSection.classList.remove(HIDDEN_CLASSNAME);
 }
 
 function clickAll() {
   console.log("전부!");
+  rightSection.classList.remove(HIDDEN_CLASSNAME);
+  leftSection.classList.remove(HIDDEN_CLASSNAME);
 }
