3f0f6484af76228771a1c982758544c68376949d
김형겸 <97586683+Brokyeom@users.noreply.github.com>
2022-10-13 20:20:21 +0900

    feat : 드롭다운 옵션 선택 이벤트 구현

--- week2/velog-clone/velogClone.js
+++ week2/velog-clone/velogClone.js
@@ -2,21 +2,43 @@ $ = (selector) => document.querySelector(selector);
 $$ = (selector) => document.querySelectorAll(selector);
 
 const HIDDEN_CLASSNAME = "hidden";
+const SELECTED_CLASSNAME = "selected";
+
 const dropDownButton = $(".nav__dropdown");
+const WriteButton = $(".header__write-button");
+const dropDownList = $(".dropdown-list");
+const dropDownOptionAll = $$(".list");
+const articleCard = $$(".article__card");
+
+// write 페이지로 이동
+function moveWritePage() {
+  location.href = "write.html";
+}
 
 // 드롭다운 메뉴 띄우기
 function openDropDown() {
-  const dropDownList = $(".dropdown-list");
   dropDownList.classList.toggle(HIDDEN_CLASSNAME);
 }
 
+// 드롭다운 옵션 선택 이벤트
+function handleDropDownOption(e) {
+  let previousOption = $(".selected");
+  const selectedOption = e.currentTarget;
+
+  dropDownButton.textContent = selectedOption.textContent;
+  previousOption.classList.remove(SELECTED_CLASSNAME);
+  selectedOption.classList.add(SELECTED_CLASSNAME);
+  previousOption = selectedOption;
+}
+
 // 게시글 모달참 구현
-const articleCard = $$(".article__card");
-
-function openModal(e) {
-  e.preventDefault();
-
+function openModal() {
   alert("모달 열려라!");
 }
+
 dropDownButton.addEventListener("click", openDropDown);
 articleCard.forEach((item) => item.addEventListener("click", openModal));
+WriteButton.addEventListener("click", moveWritePage);
+dropDownOptionAll.forEach((option) =>
+  option.addEventListener("click", handleDropDownOption)
+);
