ee10a313f023c3cbe5db836833a4c60fa781c37a
김형겸 <97586683+Brokyeom@users.noreply.github.com>
2022-10-13 20:46:58 +0900

    feat : 태그 생성 및 삭제 기능 구현

---
+++ week2/velog-clone/write.js
@@ -0,0 +1,24 @@
+const $ = (selector) => document.querySelector(selector);
+const $$ = (selector) => document.querySelectorAll(selector);
+
+const tagList = $(".tag-list");
+const tag = $(".tag");
+const TagInput = $(".tag-input");
+
+function createTag(e) {
+  if (e.keyCode == 13) {
+    const newTag = document.createElement("li");
+    newTag.classList.add("tag");
+    newTag.innerText = e.target.value;
+    newTag.addEventListener("click", deleteTag);
+    tagList.appendChild(newTag);
+    console.log(e.target.value);
+    e.target.value = "";
+  }
+}
+
+function deleteTag(e) {
+  tagList.removeChild(e.currentTarget);
+}
+
+TagInput.addEventListener("keydown", createTag);
