0d35a141d3a4d7b9bc51c7d33d649fada97007f8
김형겸 <97586683+Brokyeom@users.noreply.github.com>
2022-10-13 21:00:46 +0900

    chore : 기능 주석 추가

--- week2/velog-clone/write.js
+++ week2/velog-clone/write.js
@@ -5,6 +5,7 @@ const tagList = $(".tag-list");
 const tag = $(".tag");
 const tagInput = $(".tag-input");
 
+// 새로운 태그 생성
 function createTag(e) {
   if (e.keyCode == 13) {
     const newTag = document.createElement("li");
@@ -16,6 +17,7 @@ function createTag(e) {
   }
 }
 
+//태그 제거
 function deleteTag(e) {
   tagList.removeChild(e.currentTarget);
 }
