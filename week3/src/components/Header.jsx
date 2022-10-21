6a16df72f107ba57357580222295d937b73e2fa1
김형겸 <97586683+Brokyeom@users.noreply.github.com>
2022-10-22 03:09:09 +0900

    chore : 타이틀 변경

--- week3/src/components/Header.jsx
+++ week3/src/components/Header.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 function Header() {
   return (
     <StHeaderWrapper>
-      <StHeaderTitle>This is Titie</StHeaderTitle>
+      <StHeaderTitle>4세대 걸그룹 어디까지 알고있니??</StHeaderTitle>
     </StHeaderWrapper>
   );
 }
@@ -16,8 +16,8 @@ const StHeaderWrapper = styled.header`
 `;
 
 const StHeaderTitle = styled.h1`
-  margin-top: 2rem;
+  margin-top: 4rem;
   margin-bottom: 2rem;
 
-  font-size: 4rem;
+  font-size: 3rem;
 `;
