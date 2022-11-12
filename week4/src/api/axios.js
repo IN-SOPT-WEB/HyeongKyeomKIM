7c380f0a32b9c750aea986b27cc6cb18517649a3
김형겸 <97586683+Brokyeom@users.noreply.github.com>
2022-11-12 12:36:51 +0900

    code review : 불필요한 try.. catch 문 삭제

--- week4/src/api/axios.js
+++ week4/src/api/axios.js
@@ -1,10 +1,6 @@
 import axios from "axios";
 
 export const getUserInfo = async (username) => {
-  try {
-    const res = await axios.get(`https://api.github.com/users/${username}`);
-    return res.data;
-  } catch (error) {
-    console.error(error);
-  }
+  const res = await axios.get(`https://api.github.com/users/${username}`);
+  return res.data;
 };
